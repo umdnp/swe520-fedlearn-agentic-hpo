e3dfc6602b4937d225a174e958b4fc07f1c90976
Jim Prantzalos <233358172+umdnp@users.noreply.github.com>
2026-02-23 00:42:19 -0500

    newer flower version moved shared config to .flwr/config.toml file

--- bin/run_static_hpo.sh
+++ bin/run_static_hpo.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
+# WARNING : Ray support on Windows is experimental and may not work as expected.
+# On Windows, Flower Simulations run best in WSL2
+
 PROJECT_DIR=~/git
 VENV_DIR=~/venv
 
 export PYTHONPATH=$PROJECT_DIR/src
 export RAY_ACCEL_ENV_VAR_OVERRIDE_ON_ZERO=0
+export FLWR_HOME=$PROJECT_DIR/.flwr
 
 source ${VENV_DIR}/activate
 
 echo "Testing Static HPO Configuration ..."
 cd $PROJECT_DIR || exit
-flwr run . --run-config "experiment=static_hpo"
+
+flwr run . --run-config "experiment='static_hpo'"
